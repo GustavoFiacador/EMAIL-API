80efad806660d34d756e53097618305da24fbe8d
unknown <gfiacador@gmail.com>
2021-03-01 16:05:07 -0300

    post

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,12 @@ import express, { request, response } from 'express';
 
 const app = express();
 
-app.get("/users", (request, response) => {
-    return response.send("Hello Word")
+app.get("/", (request, response) => {
+    return response.json({message:"Metodo Get"})
+});
+
+app.post("/",(request, response) => {
+    return response.json({message:"Metodo Post"})
 });
 
 app.listen(3333, () => console.log('Server is runnig!'))
